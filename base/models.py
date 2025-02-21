ced5b101c51ffd6964dd3467f90683ee25ac88ea
Bakir Bukvic <baxoonb980@gmail.com>
2025-02-21 21:07:37 +0100

    add user model and tempalte code for leveling

--- base/models.py
+++ base/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserLevel(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    level = models.ForeignKey(UserLevel, on_delete=models.SET_NULL, null=True)
+    km_passed = models.IntegerField(default=0)
+    number_of_rides = models.IntegerField(default=0)
+    profile_description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+    class Meta:
+        verbose_name = 'User Profile'
+        verbose_name_plural = 'User Profiles'
