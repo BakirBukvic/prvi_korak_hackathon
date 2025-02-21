3cc50755784b448aaad0ce4f6f30aa8e19292bfe
Bakir Bukvic <baxoonb980@gmail.com>
2025-02-21 22:18:28 +0100

    add relevant svgs for base

--- base/models.py
+++ base/models.py
@@ -4,10 +4,11 @@ from django.contrib.auth.models import User
 class UserLevel(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
+    points = models.IntegerField(default=0)
+    svg_path = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     level = models.ForeignKey(UserLevel, on_delete=models.SET_NULL, null=True)
