2e4243a8350358d7650399a5c4522a829d4b2f6c
Bakir Bukvic <baxoonb980@gmail.com>
2025-02-22 08:49:27 +0100

    hardcode longitude and latitude

--- rides/views.py
+++ rides/views.py
@@ -15,8 +15,23 @@ class RideListView(ListView):
     model = Ride
     template_name = 'rides.html'
     context_object_name = 'rides'
-    ordering = ['-created_on']  # Orders by newest first
 
+    def get_queryset(self):
+        # Use select_related instead of prefetch_related since it's a OneToOne relationship
+        return Ride.objects.select_related('route_details').all().order_by('-created_on')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add debug information to help track data
+        context['origin_latitude'] = 44.7066641
+        context['origin_longitude'] = 18.3107132
+        context['destination_latitude'] = 43.34377480000001
+        context['destination_longitude'] = 17.8077578
+        context['selected_route_polyline'] = "qwzoGaiwnBd}@dbAhQdn@_Kpj@?n_A}g@d`Bqk@v}Aef@rw@}b@x{CoVz~C_E`fAzQd}AQ`pC_RjlBig@xy@fH`~@dfAtS`_BhwAh`Apu@dtAa@zd@fMRhIbq@lNjt@y`AlmA}`@d@kn@rWaH`w@xP~{@ws@nm@yGlh@zYvOrb@bt@gNpuAim@d_@xMbd@gk@tkAxp@ng@m@`yAv`@xi@`U`Oek@tb@mHnx@lnAfnAlJn`Aw_@tu@|~AlrAnKf}@zSf}Ar}@fn@z{Afy@rq@rQxzB|a@dtAz[|Inp@_u@|a@rGhUjjAhr@fi@duAstA`m@}_@b{@|n@d}@j~Act@h{CjLjlC~a@zq@rrAnPdv@p{@~`Avp@zj@q@|TuXds@zJrj@jjAxa@~n@|t@l]~hB|JxQebBla@a[lcAxMhaCe{AtYcAzMpYtMcMdi@n_@nZrr@za@rlAmDpl@rRrh@zNvk@}M|i@vOzUzXmCtKpPBmE`Kb[bBbe@zSoPvH~QbKvZzt@h\tRtlA|[bM~b@fr@_`@vgAs|@hn@m]hwAaYnxBm`AnaAi|@|xA{]la@yk@v`ArX`Ynd@j\pNneAv]fqBrb@`g@dg@pDj`AhBpr@sD~l@i@~u@|Tj^dg@beAnLrd@|Tjg@?jf@~^ri@aDneA|@`|@jCzYei@fGnKvMcKbD_j@dWlKlZwLjMnZbG~\j]~Hlu@ycAzWuPzVta@rl@yHd]eQ`UbKnO~RdYhw@rKhgAtQ|_Al[`}@hlArRja@`R`Wvj@tuAvq@pXfMbRnZ~x@hc@hp@rG`MxC~Y}S`o@ktAvf@u[xp@vJvXfCbSo]jn@~[zeAzs@zuA|PrdAu`@zaAz]peAwNdd@xXuDdr@^ieAbIix@vg@y_BbCu]rTF~Nbl@l_@|IvLzBbOmXl{@evAdVvHtMySnF`K`DpTvSlYhDaa@kN}AxSwi@vOcjAth@ybAdeA_rAb_@yXpQjGv`AgAtu@kBna@]d]o[hRmb@fK~GbCg|@|YibAxa@qd@jWt@vQuIhMqZvV[zVu@Auj@zC{Zr\{@|a@tHzf@rZxsAsrB}Gkc@l_@mYnHco@fXoP~b@tn@zb@I`d@_`@f_@d\zTeDf`@xQpp@zr@xT|\`l@B`f@jJxYxWdcAtyAzsAzCdx@oL`z@tGx|@ap@d{CikAty@ik@ne@ohAfYuuAv{@iu@tXk_Bz`AssBtbC{oApk@{w@lSsy@`|@wnAh_BqqAjYsSdqBlYlvBbyBhdA`wAh[x~Bt`@ba@ty@Db[dMh@vO00"
+
+        for ride in context['rides']:
+            print(f"Ride {ride.id}: Has route_details: {hasattr(ride, 'route_details')}")
+        return context
 
 class MakeRideView(CreateView):
     model = Ride
